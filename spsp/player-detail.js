9b3db8d56351d51244713afb5bec3ddf14e0437d
tosakazu <smash.tosakazu@gmail.com>
2026-07-15 18:07:29 +0900

    定期更新 2026-07-15

--- spsp/player-detail.js
+++ spsp/player-detail.js
@@ -160,9 +160,12 @@
       const lvBadge = _lv > 0
         ? `<span class="lv-badge lv-${_lv}">Lv${_lv}${_lvsfx}</span>`
         : '<span style="opacity:0.4">−</span>';
+      // 実質平日 = 実暦は土日祝だが計算上は平日扱い (プレ大会 / 大菊月等の特定シリーズ)
       const dayTag = t.is_weekend
         ? '<span class="tag-wk">休日</span>'
-        : '<span class="tag-wd">平日</span>';
+        : (t.is_weekend_real
+          ? '<span class="tag-wd" title="土日祝の開催ですが、ランキング計算上は平日大会として扱われます">実質平日</span>'
+          : '<span class="tag-wd">平日</span>');
       const smTag = '';
       const preTag = t.is_pre ? '<span class="tag-pre">プレ大会</span>' : '';
       const resTag = t.is_restricted ? '<span class="tag-res">制限</span>' : '';
